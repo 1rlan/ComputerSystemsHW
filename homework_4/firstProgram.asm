266a99bf1a9e775b20aebea93b12c2176b38f07c
Irlan Abushakhmanov <85907579+1rlan@users.noreply.github.com>
2022-10-01 01:09:36 +0300

    Add files via upload

---
+++ homework_4/firstProgram.asm
@@ -0,0 +1,1 @@
+%include "io64.inc"section .textglobal CMAINCMAIN:    mov rax, 1    mov rcx, 1    .for:        mul rcx        inc rcx        cmp rax, 0        jg .for        dec rcx    PRINT_DEC 8, rcx    xor rax, rax    ret
---
+++ homework_4/secondProgram.asm
@@ -0,0 +1,1 @@
+%include "io64.inc"section .textglobal CMAINCMAIN:    mov rax, 1    mov rcx, 1    GET_DEC 8, rbx    .for:        mul rcx        inc rcx        cmp rcx, rbx        jne .for        PRINT_DEC 8, rax    xor rax, rax    ret
