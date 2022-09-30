211f01bf85fc249a3edb67241e470d8907740636
Irlan Abushakhmanov <85907579+1rlan@users.noreply.github.com>
2022-10-01 01:11:28 +0300

    Update secondProgram.asm

--- homework_4/secondProgram.asm
+++ homework_4/secondProgram.asm
@@ -1,1 +1,16 @@
-%include "io64.inc"section .textglobal CMAINCMAIN:    mov rax, 1    mov rcx, 1    GET_DEC 8, rbx    .for:        mul rcx        inc rcx        cmp rcx, rbx        jne .for        PRINT_DEC 8, rax    xor rax, rax    ret
+%include "io64.inc"
+section .text
+global CMAIN
+CMAIN:
+    mov rax, 1
+    mov rcx, 1
+    GET_DEC 8, rbx
+    .for:
+        mul rcx
+        inc rcx
+        cmp rcx, rbx
+        jne .for
+    
+    PRINT_DEC 8, rax
+    xor rax, rax
+    ret
